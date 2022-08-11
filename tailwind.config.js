e64cb3c056f533b9ae676e92a87b8f86d5234b90
amirkalmoni <amirkalmoni@gmail.com>
2022-08-11 02:28:55 -0400

    screen config added to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      sm: "576px",
+      // => @media (min-width: 576px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "992px",
+      // => @media (min-width: 992px) { ... }
+
+      xl: "1200px",
+      // => @media (min-width: 1200px) { ... }
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/typography")],
 };
+ 
